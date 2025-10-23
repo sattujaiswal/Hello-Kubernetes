8a83bb2ab802117f7fdd60fa5a94957aa3916727
Satyendra Jaiswal <sattujaiswal@gmail.com>
2025-10-23 11:29:41 +0530

    Fix duplicate function name in FastAPI

--- main.py
+++ main.py
@@ -5,16 +5,8 @@ app = FastAPI()
 
 @app.get("/health")
 def health_check():
-    """
-    Health status endpoint.
-    Returns 200 OK with a message if the service is running fine.
-    """
     return JSONResponse(content={"status": "healthy", "message": "Service is running fine!"}, status_code=200)
 
 @app.get("/test")
-def health_check():
-    """
-    Health status endpoint.
-    Returns 200 OK with a message if the service is running fine.
-    """
-    return JSONResponse(content={"status": "Cool", "message": "I am loving k8s!"}, status_code=200)    
+def test_check():
+    return JSONResponse(content={"status": "Cool", "message": "I am loving k8s!"}, status_code=200)
